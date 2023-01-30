a76eb51a7be24598aef9cbc95c346b629286da9a
Alex VRBK <vvvorobok@gmail.com>
2023-01-30 19:34:56 +0300

    готов интерфейс

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header">
+    <h1 class="title">TODO APP</h1>
+  </header>
+  <Controller />
+  <ul class="tasks">
+    <Task />
+    <Task />
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Controller from '@/components/v-controller.vue'
+import Task from '@/components/v-task.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Controller,
+    Task
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Balsamiq Sans, cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:black;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+.header{
+  height: 80px;
+  background: #FF7B7B;
+  .title{
+    padding-top: 20px;
+    font-weight: bold;
+    font-size: 30px;
+    color: #fff;
+  }
+}
+.tasks{
+  list-style-type: none;
+  padding: 0;
+  padding-left: 10px;
+  margin: 108px auto;
+  width: 400px;
+  min-height: 400px;
 }
 </style>
---
+++ src/components/v-task.vue
@@ -0,0 +1,55 @@
+<template>
+    <li class="el">
+        <p class="task">Задача</p>
+        <button class="delete" @click="deleteEl" ><img src="@/assets/exit.png" alt="exit"></button>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'v-task'
+//   props: ['id', 'text'],
+//   methods: {
+//     deleteEl () {
+//       this.$store.commit('deleteEl', this.id)
+//     }
+//   }
+}
+</script>
+
+<style lang="scss" scoped>
+$color: #FF7B7B;
+.el{
+    margin-top: 47px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 321px;
+    height: 63px;
+    background: #FFFFFF;
+    box-shadow: -3px 6px 11px $color ;
+    border-radius: 19px;
+    .task{
+        font-weight: normal;
+        font-size: 18px;
+        color:$color;
+        width:250px;
+        text-align: left;
+        word-break: break-word;
+    }
+    button{
+        outline:none;
+        border: none;
+        background:none;
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+        img{
+            width: 35px;
+            height: 35px;
+            transition: all .3s;
+        }
+    }
+}
+</style>
